c050a21ac58abac7a01ebaa2cf8102cadbaec20a
Nitin Borse <noleclown@gmail.com>
2026-03-11 16:56:11 +0530

    Add files via upload

---
+++ ingest.py
@@ -0,0 +1,46 @@
+import os
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+BASE_DIR = r"C:\Users\lenovo\Desktop\M-CADWM_AI_Agent"
+DOCS_PATH = os.path.join(BASE_DIR, "documents")
+FAISS_PATH = os.path.join(BASE_DIR, "faiss_index")
+
+print("Loading PDFs...")
+
+documents = []
+
+for file in os.listdir(DOCS_PATH):
+
+    if file.endswith(".pdf"):
+
+        loader = PyPDFLoader(os.path.join(DOCS_PATH, file))
+        documents.extend(loader.load())
+
+print("Splitting documents...")
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800,
+    chunk_overlap=100
+)
+
+docs = splitter.split_documents(documents)
+
+print("Creating embeddings...")
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    cache_folder="models"
+)
+
+vector_db = FAISS.from_documents(docs, embeddings)
+
+print("Saving FAISS index...")
+
+vector_db.save_local(FAISS_PATH)
+
+print("Vector DB created successfully.")
